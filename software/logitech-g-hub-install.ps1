8e87fb4db8046f34d2d7d80c4a21a25aad2bc889
Kamalgurna <kamaldeep.kaur@provaltech.com>
2025-06-05 17:34:23 -0400

    Updated Logitech G HUb installation script

--- software/logitech-g-hub-install.ps1
+++ software/logitech-g-hub-install.ps1
@@ -1,2 +1,24 @@
-Start-Process -FilePath $InstallerFile -ArgumentList '--silent' -NoNewWindow -Wait
-Start-Sleep -Seconds 60
+Start-Process -FilePath $InstallerFile -ArgumentList '--silent' -NoNewWindow
+function Is-LogitechGHubInstalled {
+    $uninstallPaths = @(
+        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+        "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    )
+
+    foreach ($path in $uninstallPaths) {
+        $apps = Get-ItemProperty -Path $path -ErrorAction SilentlyContinue | Where-Object {
+            $_.DisplayName -like "*Logitech G HUB*"
+        }
+        if ($apps) {
+            return $true
+        }
+    }  return $false
+}
+
+for ($i = 1; $i -le 40; $i++) {
+    if (Is-LogitechGHubInstalled) {
+        break
+    } else {
+        Start-Sleep -Seconds 30
+    }
+}
