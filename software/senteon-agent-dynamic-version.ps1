668eac1912a64050081f0cc100726bef59d79098
Ram Kishor <100423546+Starepinjore@users.noreply.github.com>
2025-06-16 09:46:13 -0400

    Senteon Agent
    - Download Url is not returning content-deposition anymore and installing Version 1.0.0.0.
    - Dynamic Versioning is not possible with this application

--- software/senteon-agent-dynamic-version.ps1
+++
@@ -1,6 +0,0 @@
-$URL = 'https://update.senteon.co/installers/SenteonAgent.msi'
-$Result = Get-DynamicVersionFromInstallerURL $URL
-$Response = New-Object PSObject -Property @{
-    Versions = @(New-DynamicVersion -URL $URL -Version $Result.Versions.Version)
-}
-return $Response
