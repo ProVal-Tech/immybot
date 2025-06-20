1beae07a4c39389f2b43ba5e6775e04e8b3ed290
Ram Kishor <100423546+Starepinjore@users.noreply.github.com>
2025-06-20 10:47:30 -0400

    Update nvidia-app-for-enterprise-installation-script.ps1
    # Filtered to NVIDIA GPU

--- software/nvidia-app-for-enterprise-installation-script.ps1
+++ software/nvidia-app-for-enterprise-installation-script.ps1
@@ -6,16 +6,20 @@
     Override timeout: No
     Access Level: All
 #>
-#region Variables
-$argumentList = @(
-	'-s',
-	'-noreboot',
-	'-noeula',
-	'-nofinish',
-	'-nosplash'
-)
-#endRegion
+if (Get-CimInstance -ClassName Win32_VideoController | Where-Object { $_.Name -like "*NVIDIA*" -or $_.VideoProcessor -like "*NVIDIA*" }) {
+    #region Variables
+    $argumentList = @(
+        '-s',
+        '-noreboot',
+        '-noeula',
+        '-nofinish',
+        '-nosplash'
+    )
+    #endRegion
 
-#region Installation
-Start-Process -FilePath $InstallerFile -ArgumentList $argumentList -NoNewWindow -Wait
-#endRegion
+    #region Installation
+    Start-Process -FilePath $InstallerFile -ArgumentList $argumentList -NoNewWindow -Wait
+    #endRegion
+} else {
+    throw 'The NVIDIA App can only be installed on machines equipped with an NVIDIA GPU.'
+}
